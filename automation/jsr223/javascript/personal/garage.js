1ba42ddd36dadd3bc18d4082e32d6eff0f93b8d4
Jonathan Gilbert <jpg@trillica.com>
2020-04-29 20:29:27 +1000

    fixed rules to not trigger on system start

--- automation/jsr223/javascript/personal/garage.js
+++ automation/jsr223/javascript/personal/garage.js
@@ -1,18 +1,18 @@
 const { fluent, items } = require('ohj');
 
 with(fluent.withToggle) {
-    when(item('Garage_Side_Door_Occupancy').changed().to('OPEN')).
+    when(item('Garage_Side_Door_Occupancy').changed().from('CLOSED').to('OPEN')).
         then(sendOn().toItem('GarageSideDoor_Switch'), inGroup('Garage'));
 
     when(timeOfDay('NIGHT')).if(stateOfItem('Garage_Car_Door_Contact').is('OPEN')).
         then(sendOn().toItem('GarageCarDoor_Switch'), inGroup('Garage'))
  
-    when(item('Garage_Car_Door_Contact').changed().to('CLOSED')).
+    when(item('Garage_Car_Door_Contact').changed().from('OPEN').to('CLOSED')).
         or(item('Garage_Side_Door_Occupancy').changed().to('CLOSED')).
         then(sendOff().toItem('GarageLights_Switch'));
 
-    when(item('Garage_Car_Door_Contact').changed().to('OPEN')).
-        or(item('Garage_Side_Door_Occupancy').changed().to('OPEN')).
+    when(item('Garage_Car_Door_Contact').changed().from('CLOSED').to('OPEN')).
+        or(item('Garage_Side_Door_Occupancy').changed().from('CLOSED').to('OPEN')).
         if(stateOfItem('vTimeOfDay').in('NIGHT', 'SUNSET')). //todo: fluent statement
         then(sendOn().toItem('GarageLights_Switch'));
 }
