d91df86d2bba1015db0dc3255261bcfdf47e751f
Jonathan Gilbert <jpg@trillica.com>
2020-05-29 16:51:00 +1000

    added logging

--- automation/jsr223/javascript/personal/001_things_and_items.js
+++ automation/jsr223/javascript/personal/001_things_and_items.js
@@ -3,4 +3,7 @@ const logger = require('ohj').log("things_and_items")
 
 require('mythings').all()
     .then(addToSystem)
-    .catch(logger.error)
+    .catch(e => {
+        logger.error("Failed to create all tai's " + e);
+        logger.error(e.stack);
+    })
